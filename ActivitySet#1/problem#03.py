87020d84cf9d1f7788f5e630f006c808d045f0d3
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-05-22 07:54:26 +0000

    duplicate

---
+++ ActivitySet#1/problem#03.py
@@ -0,0 +1,12 @@
+# Variables, Expressions & Statements
+
+hours = input("Enter the number of hours: ")
+hrs = float(hours)
+rate = input('Enter the rate per hour: ')
+rt= float(rate)
+pay = (hrs*rt)
+print('Pay: ' +pay)
+
+
+
+
