4684a426c9d7650d29fe9b6fd0936e11cba29a44
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-05-22 06:57:30 +0000

    05

--- ActivitySet#1/problem#03.py
+++
@@ -1,8 +0,0 @@
-# Variables, Expressions & Statements
-
-hours = input("Enter the number of hours: ")
-hrs = float(hours)
-rate = input('Enter the rate per hour: ')
-rt= float(rate)
-pay = (hrs*rt)
-print('Pay: ' ,pay) 
--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,14 @@
-# Functions
-
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+score =input("Enter the Score between 0.0 and 1.0: ")
+score = float(score)
+if(score>=1.0 or score <=0.0):
+    print ("out of range") 
+elif (score >= 0.9):
+    print ("A")
+elif (score >= 0.8):
+    print ("B")
+elif (score >= 0.7):
+    print ("C")
+elif (score >= 0.6):
+    print ("D")
+elif (score < 0.6):
+    print ("F")
