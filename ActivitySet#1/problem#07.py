81ecd11637b4affac1cdb124caf333bb6a4da151
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-05-24 10:33:50 +0000

    python 06(b)

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,12 +1,18 @@
+print("Write a program that repeatedly prompts a user for integer numbers until the user enters don'. Once done is entere print out the largest and smallest of the numbers.")
 largest = None
 smallest = None
+num1 = []
 while True:
     num = input("Enter a number: ")
-    if num == "done": break
+    if num == "done":
+        break
     try:
-        num = int(num)
-        if largest is None or largest < num: largest = num
-        if smallest is None or smallest > num: smallest = num
+        num = int(num) 
+        num1.append(num)
+        if largest is None or largest < num: 
+            largest = num
+        if smallest is None or smallest > num: 
+            smallest = num
     except:
         print ("Invalid input")
         continue
