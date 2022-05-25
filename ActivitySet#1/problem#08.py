76ff315a3a7fce459eddd6b5fc8711603ceb787a
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-05-25 17:03:39 +0000

    python 07

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,6 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+print("Write code using find() and string slicing  to extract the number at the end of the line below.")
+text = "X-DSPAM-Confidence:    0.8475";
+startPos = text.find(':')
+piece = text[startPos+1:]
+end = float(piece)
+print(end)
