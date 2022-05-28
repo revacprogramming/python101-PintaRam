3a25abc0bdc058e7cf3fb95c3bf6994c425d84d4
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-05-28 16:03:06 +0000

    python 10 11

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,20 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+d=dict()
+for line in handle:
+    if not line.startswith("From "): 
+        continue
+    else:    
+        line=line.split()
+        line=line[5]
+        line=line[0:2]
+        d[line]=d.get(line,0)+1
+
+lst=list()        
+for value,count in d.items():
+    lst.append((value,count))
+
+lst.sort()
+for value,count in lst:
+	print (value,count)
