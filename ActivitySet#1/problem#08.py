f808f791451b3c390438be03350785d164211272
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-05-29 06:10:25 +0000

    practice

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,4 +1,4 @@
-print("Write code using find() and string slicing  to extract the number at the end of the line below.")
+"""Write code using find() and string slicing  to extract the number at the end of the line below."""
 text = "X-DSPAM-Confidence:    0.8475";
 startPos = text.find(':')
 piece = text[startPos+1:]
