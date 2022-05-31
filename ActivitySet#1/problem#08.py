2d2efcc4dfb562417be0ac6a8fb2f94d94d92ce7
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-05-31 10:26:13 +0000

    duplicate

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,6 +1,6 @@
 """Write code using find() and string slicing  to extract the number at the end of the line below."""
 text = "X-DSPAM-Confidence:    0.8475";
-startPos = text.find(':')
+startPos = text.find(' ')
 piece = text[startPos+1:]
 end = float(piece)
 print(end)
--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,6 +1,6 @@
 fname = input("Enter file name: ")
 if len(fname) == 0:
-    fname = open('romeo.txt')
+    fname = open(romeo.txt)
 newList = list()
 for line in fname:
     words = line.rstrip().split()
@@ -18,8 +18,8 @@ print(newList)
 
 
 
-fname = input("Enter file name: ") #Ask the user for the filename.
-fh = open(fname)                   #Open the file.
+"""fname = input("Enter file name: ") #Ask the user for the filename.
+fh = open(file)                   #Open the file.
 lst = list()                       #Create a list called lst.
 for line in fh:                    #For each line in the fh?
 	words = line.split()       #Separate the words in the line.
@@ -27,6 +27,6 @@ for word in words :           #For each word in words.
          if word not in lst :      #If word not in the lst.
             lst.append(word)       #Add the word.
          elif word in lst :            #Continue looping.
-        	continue
+            continue
 lst.sort()
-print(lst)                         #Print the lst.
+print(lst)                         #Print the lst."""
