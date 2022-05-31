7299130d50d6da69d09390d21ce38749c24cf1d0
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-05-31 10:36:38 +0000

    duplicate

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,32 +1,12 @@
-fname = input("Enter file name: ")
-if len(fname) == 0:
-    fname = open(romeo.txt)
-newList = list()
-for line in fname:
-    words = line.rstrip().split()
-    for i in words:
-        newList.append(i) 
-        newList.sort()
-print(newList)
-
-
-
-
-
-
-
-
-
-
-"""fname = input("Enter file name: ") #Ask the user for the filename.
-fh = open(file)                   #Open the file.
-lst = list()                       #Create a list called lst.
+romeo = input("Enter file name: ") #Ask the user for the filename.
+fh = open(romeo)                   #Open the file.
+lst = list()                      #Create a list called lst.
 for line in fh:                    #For each line in the fh?
-	words = line.split()       #Separate the words in the line.
-for word in words :           #For each word in words.
+	words = line.split()         #Separate the words in the line.
+	for word in words :              #For each word in words.
          if word not in lst :      #If word not in the lst.
             lst.append(word)       #Add the word.
-         elif word in lst :            #Continue looping.
-            continue
-lst.sort()
-print(lst)                         #Print the lst."""
+        # else:                    #Continue looping.
+        #	continue
+         #lst.sort()
+print(sorted(lst))                         #Print the lst.
