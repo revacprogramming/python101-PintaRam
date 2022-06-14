ac31893270d7524ffb2c6f513501cfa4356e7f26
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-06-14 10:22:34 +0000

    python 16

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,9 +1,29 @@
 # Databases
 # https://www.py4e.com/lessons/database
-url = "http://www.kite.com"
+""""url = "http://www.kite.com"
 timeout = 5
 try:
 	request = requests.get(url, timeout=timeout)
 	print("Connected to the Internet")
 except (requests.ConnectionError, requests.Timeout) as exception:
-	print("No internet connection.")
+	print("No internet connection.")""""
+
+
+
+
+
+
+
+import re
+
+hand = open("regex_sum_24962.txt")
+x=list()
+for line in hand:
+     y = re.findall('[0-9]+',line)
+     x = x+y
+
+sum=0
+for z in x:
+    sum = sum + int(z)
+
+print(sum)
