38c8798126e467f08036ca3c21d98b51a4a95ed4
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-06-14 10:28:29 +0000

    python 17

--- ActivitySet#1/problem#17.py
+++ ActivitySet#1/problem#17.py
@@ -1,4 +1,4 @@
-from socket import *
+""""from socket import *
 from codecs import decode
 host = "local host"
 port = 5000
@@ -8,4 +8,20 @@ server = socket(AF_INET, SOCK_STREAM)
 server.connect(address )
 dayandtime = decode(server.recv(bufsize), "ascii")
 print(dayandtime)
-server.close()
+server.close() """"
+
+
+
+from socket import *
+
+mysock = socket(AF_INET, SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
