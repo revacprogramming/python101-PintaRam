227c40ca2061f7e70779f6a82de34bce85c51218
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-06-30 12:08:26 +0000

    links in python

--- ActivitySet#1/problem#18.py
+++ ActivitySet#1/problem#18.py
@@ -1,6 +1,25 @@
-from googletrans import Translator
-translate = Translator()
-text = input("enter your text")
-ttext = translator.translate(text , dest = "hindi")
-print(ttext.text)
-from googletrans import translator
+rom bs4 import BeautifulSoup
+import urllib.request, urllib.parse, urllib.error
+import ssl
+import re
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+url = "http://py4e-data.dr-chuck.net/known_by_Bryce.html"
+
+#to repeat 7 times#
+for i in range(7):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    count = 0
+    for tag in tags:
+        count = count +1
+   
+        #make it stop at position 3#
+        if count>18:
+            break
+        url = tag.get('href', None)
+
+print(url)
