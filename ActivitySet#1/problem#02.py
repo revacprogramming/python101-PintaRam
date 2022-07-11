c4686e5e059c4632ffafe1651d16e0a77aca6e99
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-07-11 13:00:47 +0000

    practice

--- ActivitySet#1/problem#02.py
+++ ActivitySet#1/problem#02.py
@@ -3,7 +3,7 @@ import sqlite3
 
 #PART 1: Creating the database
 dbname = "roster.sqlite"
-conn = sqlite3.connect(dbname)
+conn = sqlite3.connect(dbname);
 cur = conn.cursor()
 
 cur.executescript('''
