a1e48e5c33b4eeb4956d8cf0c9b249997a8ff4b1
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-07-12 09:44:51 +0000

    problem 5

--- ActivitySet#2/problem#5.py
+++ ActivitySet#2/problem#5.py
@@ -1,26 +1,43 @@
 
+def get_str():
+  """get string input"""
+  name=input("Enter the name:")
+  return name
 
-def get_cs():
-    """get string input"""
+def str_to_dict(str):
+  """convert connect string to a dictionary"""
+  l=[]
+  t=tuple()
+  str=str.split(';')
+  for i in str:
+    t=tuple(i.split('='))
+    l.append(t)
+    dict={wrd[0]:wrd[1] for wrd in l}
+    ''' for wrd in l:
+    dict={wrd[0]:wrd[1]}
+    l.append((dict.items))
+    print(list)'''
+  #print(dict)
+  return dict
 
-
-def cs_to_dict(cs):
-    """convert connect string to a dictionary"""
-
-
-def dict_to_cs(d):
-    """convert a dictionary to connect string"""
-
-
+def dict_to_str(d):
+  """convert a dictionary to connect string"""
+  str=''
+  for k,v in d.items():
+    str+=k+'='+v+';'
+  return str
+  
 def main():
-    cs = get_cs()
-
-    d = cs_to_dict(cs) # convert connect string to a dictionary
-    print(d)
-
-    cs = dict_to_cs(d)
-    print(cs)
-
+   n=get_str()
+   dictionary=str_to_dict(n)
+   print(dictionary)
+   string=dict_to_str(dictionary)
+   print(string)
+  
 
 if __name__ == '__main__':
     main()
+
+'''s="a=b;c=d;e=f;g=h"
+>>> {(a:=i.split("="))[0]:a[1] for i in s.split(";")}
+{'a': 'b', 'c': 'd', 'e': 'f', 'g': 'h'}'''
