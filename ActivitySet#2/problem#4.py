ce2506685265d4a2078d90da4b1ff526f53a1771
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-07-12 09:39:25 +0000

    problem 04

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,32 +1,54 @@
-
-
-def get_cs():
+def get_string():
     """get string input"""
+    string=str(input("Enter the string :"))
+    return string
 
-
-def cs_to_lot(cs):
+def str_to_lst(str):
     """convert connected string to list of strings"""
-
-
-def lot_to_cs(lot):
+    l=[]  #l=list()
+    spt=str.split(';')
+    '''if '' in spt:
+      spt.remove('')'''
+    for wrd in spt:
+      wrd=wrd.split('=')
+      t=tuple(wrd)
+      l.append(t)
+      #print(len(l))
+    return l
+  
+def lst_to_str(lst):
     """convert list of strings to connected string"""
-
-
+    '''str=''
+    for i in lst:
+      str+=i
+    return str'''
+  
+    str=""
+    l=[]
+    for wrd in lst:
+      l.append(wrd)
+    for i in l:
+      #print(i[0],i[1],i[-2])
+      str+=i[0]+'='+i[1]+';'
+   
+    return str
+  
+print(f"Present Directory/__name__ == {__name__}")
 def main():
-    cs=get_cs()
-
-    lot=cs_to_lot(cs)  # convert connect string to list of tuples
-    print(lot)
-
-    cs=lot_to_cs(lot)  # convert list of strings to connect string
-    print(cs)
-
+    name=get_string()
+    list=str_to_lst(name)
+    string=lst_to_str(list)
+    print(list)
+    print(string)
 
 if __name__ == '__main__':
     main()
 
 
-
-
-
+'''for t in lot:
+        if t == lot[(len(lot)-1)]:
+            a = a+t[0]+'='+t[1]
+            continue
+        a = a+t[0]+'='+t[1]+';'
+    return a'''
 
