9e66b51009e18c2548b8bb28a111ed4af2561145
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-07-19 10:22:32 +0000

    problem

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,10 +1,20 @@
+class Menu():
+  """fill in class definition here"""
+  def __init__(self,item,rate):
+    self.item=item
+    self.rate=rate
+  
+  def __add__(self,other):
+    return (self.item +str(self.rate),other.item+str(other.rate))
 
 
-class Menu:
-    """fill in class definition here"""
 
 
-m = Menu()
-m = m + ("idly", 10) + ("vada", 20) ; # Hint: operator overloading special methods (__add__, __sub__, etc.)
-
-print(m)  # should print the menu properly
+m =Menu("idly", 10) 
+n=Menu("vada", 20)
+sum=m+n
+#m3=Menu('dosa',30)
+# Hint: operator overloading special methods (__add__, __sub__, etc.)
+print("__________Menu___________")  # should print the menu properly
+for i in sum:
+ print(i)
--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,11 +1,22 @@
-
-
 class Menu:
-    """fill in class definition here"""
+  """fill in class definition here"""
+  def __init__(self,item,rate):
+    self.item=item
+    self.rate=rate
+    #self.rate=int(rate)
+  
+  def __setattr__(self,item,rate):
+    print(f"You set {item} ={rate} ")
+    self.__dict__[item]=rate
+  
+m = Menu('Poori','30')
+#print(f"Item = {m.item} rate={m.rate}")
 
+m.idly=10
+m.vada=20
+m.dosa=30
+print("\033[1m Printing Menu in Dictionary Type \033[0m")
+print(m.__dict__)
+#for k,v in m.__dict__.items():
+ #print(k,v)
 
-m = Menu()
-m["idly"] = 10
-m["vada"] = 20;
-
-print(m)
